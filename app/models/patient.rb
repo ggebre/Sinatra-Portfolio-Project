e30bfdeea3623ccae3168ce15a8d6d3fcd560811
Getu Gebre <getu.gebre.gg@gmail.com>
2020-11-24 21:58:30 -0600

    check

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -10,21 +10,5 @@ class Patient < ActiveRecord::Base
           self.find {|patient| patient.user == User.find_by(id: user_id)}
       end
       
-      def doctors  
-          self.appointments.map {|appointment| Staff.find_by(id: appointment.staff_id)}
-      end
-
-      def todays_appointment 
-        # self.appointments.detect {|appointment| appointment.appointment_date.strip == todays_date}
-        self.appointments.where(appointment_date:  Time.now.strftime('%Y-%m-%d')).first
-      end
-    
-      def slug 
-        self.name.strip.gsub(/[\s\t\r\n\f]/,'-').gsub(/\W/,'-').downcase
-      end
-    
-      def self.find_by_slug(slug)
-        self.find {|patient| patient.slug == slug}
-      end 
-
+      
 end
